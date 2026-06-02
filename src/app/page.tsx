e16801dd0e232b79463479e47252ecd5552ad119
Quang Anh <lequanganh123@gmail.com>
2026-06-02 11:20:09 +0700

    database setup | Prisma and Neon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button"
+import prisma from "@/lib/db";
 
-const Page = () => {
+const Page = async () => {
+
+  const users = await prisma.user.findMany();
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(users)}
       <Button variant="outline">
         Click Me
       </Button>
